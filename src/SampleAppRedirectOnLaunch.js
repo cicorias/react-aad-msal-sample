a06420b89e63444ec958b964216c282f0cf3afbc
Omeed Musavi <omusavi@microsoft.com>
2018-04-25 15:59:33 -0700

    Add SampleAppRedirectOnLaunch code

--- src/SampleAppRedirectOnLaunch.js
+++ src/SampleAppRedirectOnLaunch.js
@@ -1,9 +1,30 @@
 import * as React from 'react';
+import * as AAD from 'react-aad-msal';
 
 class SampleAppRedirectOnLaunch extends React.Component {
-    render() {
-        return (<div />)
-    }
+
+  setUserInfo = userInfo => {
+    this.props.debugInfo = userInfo;
+  };
+
+  redirect = login => {
+    login();
+    return (<div />);
+  };
+
+  render() {
+    return (
+      <AAD.AzureAD
+        clientId=""
+        graphScopes=""
+        authority=""
+        type={AAD.LoginTypes.Redirect}
+        unauthenticatedFunction={this.redirect}
+        userInfoCallback={this.setUserInfo}>
+        <div>logged in!</div>
+      </AAD.AzureAD>
+      );
+  }
 }
 
 export default SampleAppRedirectOnLaunch;
