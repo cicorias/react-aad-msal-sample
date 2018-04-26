5cfba528f20438d1c87e529ae88eec0ac3b69d00
Laura Bochenek <labochen@microsoft.com>
2018-04-26 19:34:14 +0000

    Merged PR 17: basic functionality of popup sample
    basic functionality

--- src/SampleAppButtonLaunch.js
+++ src/SampleAppButtonLaunch.js
@@ -1,8 +1,43 @@
 import * as React from 'react';
+import { AzureAD, LoginType } from 'react-aad-msal';
 
 class SampleAppButtonLaunch extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            userInfo: null
+        }
+
+        this.userJustLoggedIn = this.userJustLoggedIn.bind(this);
+    }
+
+    unauthenticatedFunction(loginFunction) {
+        return (
+            <button onClick={loginFunction}>Login with popup</button>
+        );
+    }
+
+    userJustLoggedIn(receivedUserInfo) {
+        this.setState({ userInfo: receivedUserInfo })
+    }
+
     render() {
-        return (<div />)
+        return (
+            <AzureAD
+                clientID={process.env.REACT_APP_AAD_APP_CLIENT_ID}
+                graphScopes={[]}
+                authority={process.env.REACT_APP_AUTHORITY}
+                type={LoginType.Popup}
+                unauthenticatedFunction={this.unauthenticatedFunction}
+                userInfoCallback={this.userJustLoggedIn}>
+                <div>
+                    You're logged in!
+                    <br />
+                    {this.state.userInfo}
+                </div>
+            </AzureAD>
+        );
     }
 }
 
